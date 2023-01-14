58553280a60fb67d0b727a8c528561f898ae6f39
FarzanGit <119040324+FarzanGit@users.noreply.github.com>
2023-01-14 21:37:44 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/FarzanGit/nairobi /nairobi
+  git clone https://github.com/FarzanGit/ /tokyo
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /nairobi
+  git clone $UPSTREAM_REPO /tokyo
 fi
-cd /nairobi
+cd /tokyo
 pip3 install -U -r requirements.txt
 echo "Bot Started...."
 python3 bot.py
