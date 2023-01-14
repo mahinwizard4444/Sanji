31fc9aaae64cee3b0af04d1492b910fbacc413f8
FarzanGit <119040324+FarzanGit@users.noreply.github.com>
2023-01-14 21:46:53 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/FarzanGit/ /tokyo
+  git clone https://github.com/FarzanGit/tokyo /tokyo
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /tokyo
