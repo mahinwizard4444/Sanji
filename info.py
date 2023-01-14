bfdb726f831236ce993a7e186da8b125d2b048e5
FarzanGit <119040324+FarzanGit@users.noreply.github.com>
2023-01-14 21:05:04 +0530

    Update info.py

--- info.py
+++ info.py
@@ -59,7 +59,7 @@ CUSTOM_FILE_CAPTION = environ.get("CUSTOM_FILE_CAPTION", """ **{file_name}**(`{f
 
 2) https://t.me/+KOoNcfmnCvo4MmE1
 
-3) https://t.me/+pYhVs_XYllVlNDI1"")
+3) https://t.me/+pYhVs_XYllVlNDI1""")
 BATCH_FILE_CAPTION = environ.get("BATCH_FILE_CAPTION", CUSTOM_FILE_CAPTION)
 IMDB_TEMPLATE = environ.get("IMDB_TEMPLATE", "<b>Query: {query}</b> \n‌‌‌‌IMDb Data:\n\n🏷 Title: <a href={url}>{title}</a>\n🎭 Genres: {genres}\n📆 Year: <a href={url}/releaseinfo>{year}</a>\n🌟 Rating: <a href={url}/ratings>{rating}</a> / 10")
 LONG_IMDB_DESCRIPTION = is_enabled(environ.get("LONG_IMDB_DESCRIPTION", "False"), False)
